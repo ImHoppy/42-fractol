384dc0df124e4da0650267d1fe258f9b9d9dca60
Hoppy <mbraets@student.42.fr>
2024-05-07 18:05:54 +0200

    mlx: Build lib and add missing libc

--- mlx/build.rs
+++ mlx/build.rs
@@ -1,4 +1,16 @@
+use std::process::Command;
+
 fn main() {
+    let status = Command::new("make")
+        .current_dir("../minilibx")
+        .arg("all")
+        .status()
+        .expect("Failed to execute make command");
+
+    if !status.success() {
+        panic!("Failed to execute make command");
+    }
+
     println!("cargo:rustc-link-search=./minilibx/");
     println!("cargo:rustc-link-lib=mlx");
     println!("cargo:rustc-link-lib=Xext");
