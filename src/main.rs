a94029ebf02c16e0f6261397df8a8a27088ce97c
Hoppy <mbraets@student.42.fr>
2024-05-08 22:22:42 +0200

    Add julia fractal

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
 use mlx::{Mlx, MlxError};
 use std::process;
 
+fn julia(x: i32, y: i32, image: &mlx::MlxImage) -> u32 {
+    let mut zx = 1.5 * (x as f64 - image.width as f64 / 2.0) / (0.5 * image.width as f64);
+    let mut zy = (y as f64 - image.height as f64 / 2.0) / (0.5 * image.height as f64);
+    let mut i = 255;
+    while zx * zx + zy * zy < 4.0 && i > 1 {
+        let xtemp = zx * zx - zy * zy + 0.285;
+        zy = 2.0 * zx * zy + 0.01;
+        zx = xtemp;
+        i -= 1;
+    }
+    i
+}
+
 fn main() {
     let mlx = Mlx::new().unwrap();
 
     let width = 1080;
     let height = 720;
-    let window = mlx.new_window(width, height, "Mlx example").unwrap();
+    let window = mlx.new_window(width, height, "Fractol").unwrap();
 
     let image = match mlx.new_image(width, height) {
         Ok(img) => img,
@@ -17,6 +30,13 @@ fn main() {
     };
 
     println!("{}, {}", image.size_line, image.bits_per_pixel);
+
+    window.loop_hook(
+        move |_| {
+            println!("loop");
+        },
+        &(),
+    );
 
     window.key_hook(
         move |keycode, _| {
@@ -31,12 +51,10 @@ fn main() {
                 process::exit(0);
             // Enter
             } else if keycode == 65293 {
-                let x = width / 2;
-                let y = height / 2;
-                let color = 0xff0000;
-                for i in 0..100 {
-                    for j in 0..100 {
-                        image.pixel_put(x + i, y + j, color);
+                for y in 0..height {
+                    for x in 0..width {
+                        let color = julia(x, y, &image);
+                        image.pixel_put(x, y, color);
                     }
                 }
                 mlx.put_image_to_window(&window, &image, 0, 0);
